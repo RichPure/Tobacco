8380a48793e980bba2be83a367f93dbf3dbbd276
peiyuan.wang <peiyuan.wang@cloudsdnet.com>
2021-07-28 00:19:46 +0800

    https

--- test_fiber/main.go
+++ test_fiber/main.go
@@ -6,11 +6,9 @@ import (
 	"log"
 )
 
-
 func main() {
 	app := fiber.New()
 
-	// Field names should start with an uppercase letter
 	type SubmitRequest struct {
 		Grid      uint    `json:"grid" xml:"grid" form:"grid"`
 		Grade     uint    `json:"grade" xml:"grade" form:"grade"`
@@ -18,10 +16,11 @@ func main() {
 		Longitude float64 `json:"longitude" xml:"longitude" form:"longitude"`
 	}
 	type SubmitResponse struct {
-		 Capacity   uint    `json:"capacity" xml:"capacity" form:"capacity"`
-		 Applied    uint    `json:"applied" xml:"applied" form:"applied"`
-		 Population uint64  `json:"population" xml:"population" form:"population"`
-		 Distance   float64 `json:"distance" xml:"distance" form:"distance"`
+		Result     uint    `json:"result" xml:"result" form:"result"`
+		Capacity   uint    `json:"capacity" xml:"capacity" form:"capacity"`
+		Applied    uint    `json:"applied" xml:"applied" form:"applied"`
+		Population uint64  `json:"population" xml:"population" form:"population"`
+		Distance   float64 `json:"distance" xml:"distance" form:"distance"`
 	}
 	app.Post("/tobacco/v1/wx/submit", func(c *fiber.Ctx) error {
 		req := new(SubmitRequest)
@@ -41,16 +40,38 @@ func main() {
 		return c.SendString(string(jsonStr))
 	})
 
+	type RegisterRequest struct {
+		Business  uint    `json:"business" xml:"business" form:"business"`
+		RoomNum   uint    `json:"boomNum" xml:"boomNum" form:"boomNum"`
+		Grid      uint    `json:"grid" xml:"grid" form:"grid"`
+		Grade     uint    `json:"grade" xml:"grade" form:"grade"`
+		Latitude  float64 `json:"latitude" xml:"latitude" form:"latitude"`
+		Longitude float64 `json:"longitude" xml:"longitude" form:"longitude"`
+	}
+	type RegisterResponse struct {
+		Result uint   `json:"result" xml:"result" form:"result"`
+		Msg    string `json:"msg" xml:"msg" form:"msg"`
+	}
 	app.Post("/tobacco/v1/wx/register", func(c *fiber.Ctx) error {
-		req := new(RegisterInfo)
+		req := new(RegisterRequest)
 
 		if err := c.BodyParser(req); err != nil {
 			return err
 		}
 
-		log.Println(req.Name) // john
-		log.Println(req.Pass) // doe
+		log.Println("register start")
+		log.Println(req.Business)
+		log.Println(req.RoomNum)
+		log.Println(req.Grid)
+		log.Println(req.Grade)
+		log.Println(req.Latitude)
+		log.Println(req.Longitude)
+		log.Println("register end")
+
+		resp := RegisterResponse{Result: 0, Msg: "ok"}
+		jsonStr, _ := json.Marshal(resp)
+		return c.SendString(string(jsonStr))
 	})
 
-	app.Listen(":8888")
+	app.ListenTLS(":8888", "./6023104_www.jivvsvy.cn.pem", "./6023104_www.jivvsvy.cn.key")
 }
