3ff4a5baef1af9460d7659b30135ecd613c591fe
peiyuan.wang <peiyuan.wang@cloudsdnet.com>
2021-08-11 01:37:44 +0800

    https调试完成

--- test_fiber/main.go
+++ test_fiber/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
-	"log"
+	"github.com/golang/glog"
 )
 
-func main() {
-	app := fiber.New()
+//ccc
 
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	app := fiber.New()
+	fmt.Printf("start")
 	type SubmitRequest struct {
 		Grid      uint    `json:"grid" xml:"grid" form:"grid"`
 		Grade     uint    `json:"grade" xml:"grade" form:"grade"`
@@ -16,28 +23,27 @@ func main() {
 		Longitude float64 `json:"longitude" xml:"longitude" form:"longitude"`
 	}
 	type SubmitResponse struct {
-		Result     uint    `json:"result" xml:"result" form:"result"`
-		Capacity   uint    `json:"capacity" xml:"capacity" form:"capacity"`
-		Applied    uint    `json:"applied" xml:"applied" form:"applied"`
-		Population uint64  `json:"population" xml:"population" form:"population"`
-		Distance   float64 `json:"distance" xml:"distance" form:"distance"`
+		Result      uint    `json:"result" xml:"result" form:"result"`
+		Capacity    uint    `json:"capacity" xml:"capacity" form:"capacity"`
+		Applied     uint    `json:"applied" xml:"applied" form:"applied"`
+		Population  uint64  `json:"population" xml:"population" form:"population"`
+		Distance    float64 `json:"distance" xml:"distance" form:"distance"`
+		CompanyName string  `json:"company_name" xml:"company_name" form:"company_name"`
 	}
-	app.Post("/tobacco/v1/wx/submit", func(c *fiber.Ctx) error {
-		req := new(SubmitRequest)
 
+	app.Post("/tobacco/v1/wx/grids/submit", func(c *fiber.Ctx) error {
+		c.Accepts("application/json")
+
+		fmt.Printf("grids req=%s\n", string(c.Request().Body()))
+		req := new(SubmitRequest)
 		if err := c.BodyParser(req); err != nil {
 			return err
 		}
-		log.Println("submit start")
-		log.Println(req.Grid)
-		log.Println(req.Grade)
-		log.Println(req.Latitude)
-		log.Println(req.Longitude)
-		log.Println("submit end")
 
-		resp := SubmitResponse{Capacity: 10, Applied: 2, Population: 888, Distance: 123.456}
+		resp := SubmitResponse{Result: 0, Capacity: 10, Applied: 2, Population: 688, Distance: 123.456, CompanyName: "哈哈哈哈哈哈"}
 		jsonStr, _ := json.Marshal(resp)
-		return c.SendString(string(jsonStr))
+		fmt.Printf("grids resp=%s\n", jsonStr)
+		return c.JSON(resp)
 	})
 
 	type RegisterRequest struct {
@@ -52,26 +58,21 @@ func main() {
 		Result uint   `json:"result" xml:"result" form:"result"`
 		Msg    string `json:"msg" xml:"msg" form:"msg"`
 	}
-	app.Post("/tobacco/v1/wx/register", func(c *fiber.Ctx) error {
-		req := new(RegisterRequest)
 
+	app.Post("/tobacco/v1/wx/result/submit", func(c *fiber.Ctx) error {
+		c.Accepts("application/json")
+
+		fmt.Printf("result req=%s\n", string(c.Request().Body()))
+		req := new(RegisterRequest)
 		if err := c.BodyParser(req); err != nil {
 			return err
 		}
 
-		log.Println("register start")
-		log.Println(req.Business)
-		log.Println(req.RoomNum)
-		log.Println(req.Grid)
-		log.Println(req.Grade)
-		log.Println(req.Latitude)
-		log.Println(req.Longitude)
-		log.Println("register end")
-
 		resp := RegisterResponse{Result: 0, Msg: "ok"}
 		jsonStr, _ := json.Marshal(resp)
-		return c.SendString(string(jsonStr))
+		fmt.Printf("result end resp=%s\n", jsonStr)
+		return c.JSON(resp)
 	})
 
-	app.ListenTLS(":8888", "./6023104_www.jivvsvy.cn.pem", "./6023104_www.jivvsvy.cn.key")
+	app.ListenTLS(":443", "./6023104_www.jivvsvy.cn.pem", "./6023104_www.jivvsvy.cn.key")
 }
